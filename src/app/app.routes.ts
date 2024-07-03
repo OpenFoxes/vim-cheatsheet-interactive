8000c382f4618d3d1fa6b914262ec7c4d8286f15
Bono Fox <microfuchs@outlook.com>
2024-07-03 23:28:21 +0200

    🎉 #1: Created a keyboard-view-container
    - initially opened
    - holds and shows current mode

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { KeyboardViewComponent } from './components/keyboard-view/keyboard-view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: KeyboardViewComponent, title: 'VIM Keyboard' },
+];
---
+++ src/app/components/keyboard-view/keyboard-view.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { VimMode } from '../../model/vim-mode';
+
+/**
+ * The keyboard-view defines a container surrounding the virtual keyboard.
+ * It's containing the current mode and a legend of the colors as well as
+ * configuration options.
+ */
+@Component({
+  selector: 'app-keyboard-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './keyboard-view.component.html',
+  styleUrl: './keyboard-view.component.scss',
+})
+export class KeyboardViewComponent {
+  private _currentMode = VimMode.NORMAL;
+
+  currentModeMessage() {
+    switch (this._currentMode) {
+      case VimMode.NORMAL:
+        return `normal`;
+      case VimMode.INSERT:
+        return `insert`;
+      case VimMode.VISUAL:
+        return `visual`;
+      case VimMode.COMMAND:
+        return `command`;
+      case VimMode.REPLACE:
+        return `replace`;
+      case VimMode.BINARY:
+        return `binary`;
+    }
+  }
+
+  set currentMode(value: VimMode) {
+    this._currentMode = value;
+  }
+}
