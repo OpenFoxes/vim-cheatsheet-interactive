50cf825635aa9123afec8a98ca097fa1112e9b3d
Bono Fox <microfuchs@outlook.com>
2024-07-07 00:19:01 +0200

    ✨ #1: KeyboardService holds a keyboard layout

--- src/app/services/keyboard.service.ts
+++ src/app/services/keyboard.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@angular/core';
+import { KeyboardKeySlot } from '../model/keyboard-key-slot';
+import { LiveKeyboardLayout } from '../model/keyboard-layout.type';
+
+/**
+ * Special options for defining the keyboard structure
+ *
+ * @see defineSlots
+ */
+type SlotDefinitionOptions = {
+  /** Number of slots for each row of the keyboard */
+  allRowslots: number[];
+};
 
 /**
  * Service for creating and configuring a keyboard model
@@ -7,5 +19,29 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class KeyboardService {
-  constructor() {}
+  private _keyboard: LiveKeyboardLayout = { rows: [] };
+
+  /**
+   * Current keyboard
+   */
+  get keyboard() {
+    return { ...this._keyboard };
+  }
+
+  /**
+   * definition of the basic keyboard structure without bindings
+   * @param options
+   */
+  defineSlots(options: SlotDefinitionOptions) {
+    this._keyboard.rows = [];
+    options.allRowslots.forEach((rowslots) => {
+      const keyslots: KeyboardKeySlot[] = [];
+
+      for (let slotId = 0; slotId < rowslots; slotId++) {
+        keyslots.push(new KeyboardKeySlot());
+      }
+
+      this._keyboard.rows.push({ keyslots });
+    });
+  }
 }
