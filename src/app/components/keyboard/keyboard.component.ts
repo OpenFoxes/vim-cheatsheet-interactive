173d805dc7f775fd48c321fe21ea4b2719a9dbed
Bono Fox <microfuchs@outlook.com>
2024-07-06 13:51:28 +0200

    ✨ #1: component for displaying a virtual keyboard
    - takes a keyboard layout and transforms it into a map of keyboard-keys
    - displays the map

---
+++ src/app/components/keyboard/keyboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-keyboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './keyboard.component.html',
+  styleUrl: './keyboard.component.scss'
+})
+export class KeyboardComponent {
+
+}
