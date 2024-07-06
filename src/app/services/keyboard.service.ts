740d92d74a0735e5c8fce709e1b1dd788f270ba3
Bono Fox <microfuchs@outlook.com>
2024-07-07 01:10:53 +0200

    ✨ #1: Bind keys to virtual keyboard

--- src/app/services/keyboard.service.ts
+++ src/app/services/keyboard.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { KeyboardKeySlot } from '../model/keyboard-key-slot';
-import { LiveKeyboardLayout } from '../model/keyboard-layout.type';
+import {
+  KeyboardBindingLayout,
+  LiveKeyboardLayout,
+} from '../model/keyboard-layout.type';
 
 /**
  * Special options for defining the keyboard structure
@@ -10,6 +13,20 @@ import { LiveKeyboardLayout } from '../model/keyboard-layout.type';
 type SlotDefinitionOptions = {
   /** Number of slots for each row of the keyboard */
   allRowslots: number[];
+};
+
+/**
+ * Special options for binding a layout to the keyboard structure
+ *
+ * @see defineSlots
+ */
+type SlotBindingOptions = {
+  /** Keys to bind to the existing slots */
+  layout: KeyboardBindingLayout;
+  /** Binding offsets for each row */
+  offsets?: number[];
+  /** Vertical binding offsets between rows */
+  verticalOffsets?: number[];
 };
 
 /**
@@ -43,5 +60,98 @@ export class KeyboardService {
 
       this._keyboard.rows.push({ keyslots });
     });
+  }
+
+  /**
+   * binding a layout to the existing keyboard structure
+   * @param options
+   */
+  bindToSlots(options: SlotBindingOptions): boolean {
+    const bindings = options.layout;
+
+    // check lengths
+    let error = false;
+
+    const totalVerticalOffsets =
+      options.verticalOffsets?.reduce(
+        (accumulator, currentValue) => accumulator + currentValue,
+        0,
+      ) ?? 0;
+    if (
+      bindings.rows.length + totalVerticalOffsets >
+      this._keyboard.rows.length
+    ) {
+      return false;
+    }
+
+    let rowId = 0;
+    let verticalOffsetCounter = 0;
+    let verticalOffsetNumber = 0;
+    this._keyboard.rows.forEach((row) => {
+      if (
+        verticalOffsetCounter <
+        (options.verticalOffsets !== undefined
+          ? options.verticalOffsets[verticalOffsetNumber]
+          : 0)
+      ) {
+        verticalOffsetCounter++;
+      } else {
+        verticalOffsetCounter = 0;
+        verticalOffsetNumber++;
+        const currentRowBindings = bindings.rows[rowId];
+        if (currentRowBindings !== undefined) {
+          const totalHorizontalOffsets =
+            options.offsets !== undefined ? options.offsets[rowId] : 0;
+          if (
+            currentRowBindings.keys.length + totalHorizontalOffsets >
+            row.keyslots.length
+          ) {
+            error = true;
+          }
+          rowId++;
+        }
+      }
+    });
+
+    if (error) {
+      return false;
+    }
+
+    // bind keys
+    rowId = 0;
+    verticalOffsetCounter = 0;
+    verticalOffsetNumber = 0;
+    this._keyboard.rows.forEach((row) => {
+      if (
+        verticalOffsetCounter <
+        (options.verticalOffsets !== undefined
+          ? options.verticalOffsets[verticalOffsetNumber]
+          : 0)
+      ) {
+        verticalOffsetCounter++;
+      } else {
+        verticalOffsetCounter = 0;
+        verticalOffsetNumber++;
+        const currentRowBindings = bindings.rows[rowId];
+        if (currentRowBindings !== undefined) {
+          let slotId = 0;
+          let offsetCounter = 0;
+          row.keyslots.forEach((keyslot) => {
+            if (
+              offsetCounter <
+              (options.offsets !== undefined ? options.offsets[rowId] : 0)
+            ) {
+              offsetCounter++;
+            } else {
+              keyslot.key = currentRowBindings.keys[slotId];
+              slotId++;
+            }
+          });
+          rowId++;
+        }
+      }
+    });
+
+    return true;
   }
 }
