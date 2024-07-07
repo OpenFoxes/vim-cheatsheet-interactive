040322a69e1a82e83527a286cd44dfdf1015b6e3
Bono Fox <microfuchs@outlook.com>
2024-07-07 19:31:41 +0200

    ✨ #1: Component for displaying a keyboard

--- src/app/components/keyboard/keyboard.component.ts
+++ src/app/components/keyboard/keyboard.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { KeyboardService } from '../../services/keyboard.service';
 
 @Component({
   selector: 'app-keyboard',
   standalone: true,
   imports: [],
   templateUrl: './keyboard.component.html',
-  styleUrl: './keyboard.component.scss'
+  styleUrl: './keyboard.component.scss',
 })
 export class KeyboardComponent {
+  constructor(private readonly keyboardService: KeyboardService) {}
 
+  protected get keyboardRows() {
+    return [...this.keyboardService.keyboard.rows];
+  }
 }
