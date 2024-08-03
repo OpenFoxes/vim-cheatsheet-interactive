f207241fbc223242727005e01e8a84ff2584e556
Bono Fox <microfuchs@outlook.com>
2024-08-03 21:06:13 +0200

    🏷️ #1: class for the definition of keybindings
    - will be used to bind keys to operations

---
+++ src/app/model/minimal-vim-key-binding.ts
@@ -0,0 +1,21 @@
+type MinimalVimKeyBinding = {};
+
+export interface VimSoloKeyBinding extends MinimalVimKeyBinding {
+  primaryKey: string;
+  alternateKeys?: ReadonlyArray<string>;
+}
+
+export interface VimMultiKeyBinding extends MinimalVimKeyBinding {
+  primaryKeys: ReadonlyArray<string>;
+  alternateKeys?: ReadonlyArray<string>;
+}
+
+export interface VimSequenceBinding extends MinimalVimKeyBinding {
+  primarySequence: string;
+  alternateSequences?: ReadonlyArray<string>;
+}
+
+export type AnyVimKeyBinding =
+  | VimSoloKeyBinding
+  | VimMultiKeyBinding
+  | VimSequenceBinding;
