ed47ae8446433bf5a7c104c167f09745adc19d60
Bono Fox <microfuchs@outlook.com>
2024-08-03 21:03:16 +0200

    🏷️ #1: collected classifications for vim operations
    - classification on the needed level of experience
    - classification on the importance in everyday usage

---
+++ src/app/model/vim-operation-classifications.enum.ts
@@ -0,0 +1,21 @@
+/**
+ * Estimated levels of experience with VIM
+ */
+export enum VimOperationExperience {
+  FIRST_STEPS,
+  BEGINNER,
+  ADVANCED_BEGINNER,
+  COMPETENT,
+  PROFESSIONAL,
+}
+
+/**
+ * Estimated importance of VIM-operations in everyday usage
+ */
+export enum VimOperationImportance {
+  REQUIRED,
+  IMPORTANT,
+  SEMI_IMPORTANT,
+  NICE_TO_HAVE,
+  UNIMPORTANT,
+}
